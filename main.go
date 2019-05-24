c07f960ab9ff5f64e8f6842b39511aa6702a54eb
MaoJie <maojie9@gmail.com>
2019-05-24 11:19:49 +0800

    Try to add new function for the pb ratio but still implementing
    Change the struct to Capital letters so that external go files can use

--- main.go
+++ main.go
@@ -8,5 +8,22 @@ import (
 
 func main() {
 	var s = generatedata.GenerateCompanyInformation()
+
+	//Show the raw data
 	fmt.Println(s)
+
+	//Generate P/B ratio
+	generatePB(s)
+
+}
+
+func generatePB(s []generatedata.Company) {
+
+	for i := 0; i < len(s); i++ {
+		//Generate book value
+		companyName := s[i]
+
+		fmt.Println(companyName)
+	}
+
 }
